567bd7c91ce231f6936dfa8890a257d88e09792f
Mugdha Kulkarni <mukulkarni@ebay.com>
2017-02-20 18:08:59 -0800

    Updated to use the DBFlow ORM library to use a database to save data from SimpleTodo

---
+++ SimpleToDo/app/src/main/java/com/example/mukulkarni/simpletodo/todo/Task.java
@@ -0,0 +1,26 @@
+package com.example.mukulkarni.simpletodo.todo;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+/**
+ * Created by mukulkarni on 2/20/17.
+ */
+
+@Table(database = TaskDataBase.class)
+public class Task extends BaseModel {
+    @PrimaryKey
+    @Column
+    private String task;
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getTask()
+    {
+        return task;
+    }
+}
---
+++ SimpleToDo/app/src/main/java/com/example/mukulkarni/simpletodo/todo/TaskDataBase.java
@@ -0,0 +1,14 @@
+package com.example.mukulkarni.simpletodo.todo;
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+/**
+ * Created by mukulkarni on 2/20/17.
+ */
+
+@Database(name = TaskDataBase.NAME, version = TaskDataBase.VERSION)
+public class TaskDataBase {
+    public static final String NAME = "TaskDataBase";
+
+    public static final int VERSION = 1;
+}
